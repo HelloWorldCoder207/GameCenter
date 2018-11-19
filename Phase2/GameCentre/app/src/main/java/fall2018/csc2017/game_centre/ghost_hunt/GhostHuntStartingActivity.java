51442006970985a29c5bfabc503feb71e774e676
zuoqiwx <zuoqiwchris@gmail.com>
2018-11-19 14:39:23 -0500

    Locked screen orientation for GhostHuntGameActivity in Manifest. Updated components in GhostHuntStartingActivity.

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/game_centre/ghost_hunt/GhostHuntStartingActivity.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/game_centre/ghost_hunt/GhostHuntStartingActivity.java
@@ -4,12 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
 import java.util.Map;
 
+import fall2018.csc2017.game_centre.CurrentStatus;
 import fall2018.csc2017.game_centre.GameCentreActivity;
 import fall2018.csc2017.game_centre.R;
 
@@ -45,6 +53,33 @@ public class GhostHuntStartingActivity extends AppCompatActivity {
     }
 
     /**
+     * Dispatch onResume() to fragments.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // TODO: resume activity
+    }
+
+    /**
+     * Dispatch onPause() to fragments.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // TODO: pause activity
+    }
+
+    /**
+     * Take care of popping the fragment back stack or finishing the activity
+     * as appropriate.
+     */
+    @Override
+    public void onBackPressed() {
+        switchToGameCentre();
+    }
+
+    /**
      * Activate back button.
      */
     private void addBackButtonListener() {
@@ -66,6 +101,7 @@ public class GhostHuntStartingActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // TODO: start game
+                switchToGame();
             }
         });
     }
@@ -78,14 +114,7 @@ public class GhostHuntStartingActivity extends AppCompatActivity {
         loadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                loadFromFile(SAVE_FILENAME);
-//                saveToFile(TEMP_SAVE_FILENAME);
-                if (boardManager == null) {
-                    makeToastText("No previous saved game");
-                } else {
-                    makeToastText("Loaded Game");
-                    switchToGame();
-                }
+                // TODO: load game
             }
         });
     }
@@ -98,9 +127,20 @@ public class GhostHuntStartingActivity extends AppCompatActivity {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                loadFromFile(TEMP_SAVE_FILENAME);
-//                saveToFile(SAVE_FILENAME);
-                makeToastText("Game Saved");
+                // TODO: save game
+            }
+        });
+    }
+
+    /**
+     * Activate Scoreboard Button.
+     */
+    private void addScoreboardListener() {
+        Button scoreboard = findViewById(R.id.Scoreboard);
+        scoreboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchToScoreboard();
             }
         });
     }
@@ -138,5 +178,50 @@ public class GhostHuntStartingActivity extends AppCompatActivity {
         Intent tmp = new Intent(this, GhostHuntScoreboardActivity.class);
         // TODO: go to scoreboard
         startActivity(tmp);
+    }
+
+    /**
+     * Load the board manager from fileName.
+     *
+     * @param fileName the name of the file
+     */
+    private void loadFromFile(String fileName) {
+        try {
+            InputStream inputStream = this.openFileInput(fileName);
+            if (inputStream != null) {
+                ObjectInputStream input = new ObjectInputStream(inputStream);
+                boardManagerMap = (HashMap<String, BoardManager>) input.readObject();
+                boardManager = boardManagerMap.get(CurrentStatus.getCurrentUser().getUsername());
+                inputStream.close();
+            }
+        } catch (FileNotFoundException e) {
+            Log.e(LOG_TAG, "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Can not read file: " + e.toString());
+        } catch (ClassNotFoundException e) {
+            Log.e(LOG_TAG, "File contained unexpected data type: " + e.toString());
+        } catch (NullPointerException e) {
+            Log.e(LOG_TAG, "Calling on null reference: " + e.toString());
+        }
+    }
+
+    /**
+     * Save the board manager to fileName.
+     *
+     * @param fileName the name of the file
+     */
+    private void saveToFile(String fileName) {
+        try {
+            if (boardManagerMap == null) {
+                boardManagerMap = new HashMap<>();
+            }
+            boardManagerMap.put(CurrentStatus.getCurrentUser().getUsername(), boardManager);
+            ObjectOutputStream outputStream = new ObjectOutputStream(
+                    this.openFileOutput(fileName, MODE_PRIVATE));
+            outputStream.writeObject(boardManagerMap);
+            outputStream.close();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "File write failed: " + e.toString());
+        }
     }
 }
