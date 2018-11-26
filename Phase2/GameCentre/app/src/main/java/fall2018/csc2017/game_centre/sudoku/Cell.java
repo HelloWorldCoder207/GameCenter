b039f73dfd00f9b43e7f02bb4421a27ae23c9159
Falanke21 <1065314944@qq.com>
2018-11-26 13:55:37 -0500

    added javadoc for three sudoku classes

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/game_centre/sudoku/Cell.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/game_centre/sudoku/Cell.java
@@ -1,27 +1,60 @@
 package fall2018.csc2017.game_centre.sudoku;
 
+/**
+ * Cell in sudoku board.
+ */
 class Cell {
+    /**
+     * the background id of the cell.
+     */
     int background;
+
+    /**
+     * determine if the cell is visible, i.e. if the cell is displaying the number or a blank.
+     */
     private boolean isVisible;
+
+    /**
+     * The value of the cell, from 1-9
+     */
     private int value;
 
+    /**
+     * Constructor of the cell.
+     * @param value     the preset value of the cell.
+     * @param isVisible the visibility of the cell.
+     */
     Cell(int value, boolean isVisible){
         this.value = value;
         this.isVisible = isVisible;
     }
 
+    /**
+     * Setter for the background, should be called from an Activity class.
+     * @param background the id of the background.
+     */
     void setBackground(int background) {
         this.background = background;
     }
 
+    /**
+     * Getter for value.
+     * @return the value of the cell.
+     */
     int getValue() {
         return value;
     }
 
+    /**
+     * Change the invisible cell into visible.
+     */
     void changeToVisible(){
         this.isVisible = true;
     }
 
+    /**
+     * String representation of the cell.
+     */
     public String toString(){
         if (isVisible){
             return String.valueOf(value);
--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/game_centre/sudoku/SudokuGameState.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/game_centre/sudoku/SudokuGameState.java
@@ -1,5 +1,26 @@
 package fall2018.csc2017.game_centre.sudoku;
 
+import fall2018.csc2017.game_centre.GameTimer;
+
+/**
+ * Game state of sudoku. Contains sudoku board, hint counter, game time etc.
+ */
 public class SudokuGameState {
     //TODO add sudoku board, hint counter, game time,
+    /**
+     * Board of sudoku.
+     */
+    private SudokuBoard board;
+
+    /**
+     * The number of remaining hints. Set to 3 initial.
+     */
+    private int hintCounter;
+
+    /**
+     * The timer for the game.
+     */
+    private GameTimer timer;
+
+
 }
