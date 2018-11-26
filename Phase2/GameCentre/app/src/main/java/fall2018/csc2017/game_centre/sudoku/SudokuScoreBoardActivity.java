18463382ff29a052e00850668865fd95d486199f
Lu Chen Zhong <williamcz.lu@mail.toronto.ca>
2018-11-26 14:25:24 -0500

    Added ScoreBoard feature to Sudoku, SudokuScoreBoard contains the score algorithms, SudokuScoreBoardActivity connects with the xml files, and FileHandles deals with corresponding IO.

---
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/game_centre/sudoku/SudokuScoreBoardActivity.java
@@ -0,0 +1,125 @@
+package fall2018.csc2017.game_centre.sudoku;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Map;
+
+import fall2018.csc2017.game_centre.CurrentStatus;
+import fall2018.csc2017.game_centre.R;
+import fall2018.csc2017.game_centre.User;
+import fall2018.csc2017.game_centre.UserFileHandler;
+
+/**
+ * Scoreboard of Sudoku.
+ */
+public class SudokuScoreBoardActivity extends AppCompatActivity {
+
+    /**
+     * The file handler for user file io
+     */
+    private UserFileHandler userFileHandler = UserFileHandler.getInstance();
+
+    /**
+     * The file handler for score file io.
+     */
+    private SudokuScoreBoardFileHandler scoreFileHandler = SudokuScoreBoardFileHandler.getInstance();
+
+    /**
+     * ScoreBoard class for game.
+     */
+    private static SudokuScoreBoard scoreBoard = new SudokuScoreBoard();
+
+    /**
+     * Users, loaded from SAVE_ALL_USERS type-casted into a Hashmap
+     */
+    private Map<String, User> users = userFileHandler.getUsers();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scoreboard);
+        scoreFileHandler.loadFromScoreFile(this);
+        if (!((getIntent().getExtras()) == null)) {
+            int move = (int) getIntent().getExtras().get("move");
+            int time = (int) getIntent().getExtras().get("totalTime");
+            ArrayList updateParam =
+                    new ArrayList<>(Arrays.asList(move, time));
+            update(updateParam);
+        }
+        scoreBoard.formatUsers(users, scoreFileHandler.leaderBoard); // sorts user information and prepares them for display
+        scoreFileHandler.saveToScoreFile(this, scoreFileHandler.SAVE_SCOREBOARD);
+        addTopFivePlayersTextView();
+    }
+
+    /**
+     * Generate appropriate text for TextView Display
+     * @return String in the format of "username": "points"
+     */
+    private String generateText(int index){
+        String tvDisplay = String.format(Locale.CANADA, "%s : %s points",
+                scoreFileHandler.leaderBoard.get(index).get(1), scoreFileHandler.leaderBoard.get(0).get(0));
+        return tvDisplay;
+    }
+
+    /**
+     * Add TextView for top five players
+     */
+    private void addTopFivePlayersTextView() {
+        ArrayList<String> displayText = new ArrayList<>();
+        for (int i = 0; i < scoreFileHandler.leaderBoard.size(); i++){
+            displayText.add(generateText(i));
+        }
+        TextView tvFirst = findViewById(R.id.first);
+        tvFirst.setText(displayText.get(0));
+        TextView tvSecond = findViewById(R.id.second);
+        String secondDisplay = (scoreFileHandler.leaderBoard.size() > 1)? generateText(1) : "No data recorded.";
+        tvSecond.setText(secondDisplay);
+        TextView tvThird = findViewById(R.id.third);
+        String thirdDisplay = (scoreFileHandler.leaderBoard.size() > 2)? generateText(2) : "No data recorded.";
+        tvThird.setText(thirdDisplay);
+        TextView tvFourth = findViewById(R.id.fourth);
+        String fourthDisplay = (scoreFileHandler.leaderBoard.size() > 3)? generateText(3) : "No data recorded.";;
+        tvFourth.setText(fourthDisplay);
+        TextView tvFifth = findViewById(R.id.fifth);
+        String fifthDisplay = (scoreFileHandler.leaderBoard.size() > 4)? generateText(4) : "No data recorded.";;
+        tvFifth.setText(fifthDisplay);
+        TextView tvCurrentPlayer = findViewById(R.id.player_rank);
+        String currentDisplay = generateCurrentText();
+        tvCurrentPlayer.setText(currentDisplay);
+    }
+
+    /**
+     * Generate the current player's rank and points
+     * @return the current player's rank and points
+     */
+    private String generateCurrentText(){
+        String currentDisplay = ("No data recorded, yet.");
+        User currentUser = CurrentStatus.getCurrentUser();
+        for (int index = 0; index < scoreFileHandler.leaderBoard.size(); index++) {
+            if (scoreFileHandler.leaderBoard.get(index).get(1).equals(currentUser.getUsername())) {
+                currentDisplay = String.format(Locale.CANADA, "%s : %s points, rank %d",
+                        scoreFileHandler.leaderBoard.get(index).get(1),
+                        scoreFileHandler.leaderBoard.get(index).get(0), index + 1);
+                break;
+            }
+        }
+        return currentDisplay;
+    }
+
+    /**
+     * Update the scoreFileHandler.leaderBoard.
+     * @param scoreParameter the array list of information needed for score.
+     */
+    public void update(ArrayList<Integer> scoreParameter) {
+        int newScore = scoreBoard.calculateScore(scoreParameter);
+        scoreBoard.update(newScore, users);
+        scoreFileHandler.saveToScoreFile(this, scoreFileHandler.SAVE_SCOREBOARD);
+        scoreFileHandler.loadFromScoreFile(this);
+        scoreBoard.formatUsers(users, scoreFileHandler.leaderBoard);
+        addTopFivePlayersTextView();
+    }
+}
---
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/game_centre/sudoku/SudokuScoreBoardFileHandler.java
@@ -0,0 +1,88 @@
+package fall2018.csc2017.game_centre.sudoku;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import static android.content.Context.MODE_PRIVATE;
+
+public class SudokuScoreBoardFileHandler {
+    /**
+     * The save file
+     */
+    public final String SAVE_SCOREBOARD = "save_sudoku_scoreboard.ser";
+    /**
+     * The fileSaver instance.
+     */
+    private static SudokuScoreBoardFileHandler fileSaver;
+
+    /**
+     * leader board, loaded from SAVE_SCOREBOARD, type-casted into a nested ArrayList
+     * Each sub ArrayList denotes a "tuple" of two elements, with the score being the first and
+     * user name being the second.
+     */
+    public ArrayList<ArrayList> leaderBoard;
+
+    /**
+     * Private constructor for singleton.
+     */
+    private SudokuScoreBoardFileHandler() {
+    }
+
+    /**
+     * The getter for Singleton File Saver.
+     *
+     * @return the file saver as needed
+     */
+    static SudokuScoreBoardFileHandler getInstance() {
+        if (fileSaver == null) {
+            fileSaver = new SudokuScoreBoardFileHandler();
+        }
+        return fileSaver;
+    }
+
+    /**
+     * Load the scoreboard from SAVE_SCOREBOARD.
+     */
+    public void loadFromScoreFile(Context context) {
+        try {
+            InputStream inputStream = context.openFileInput(SAVE_SCOREBOARD);
+            if (inputStream != null) {
+                ObjectInputStream input = new ObjectInputStream(inputStream);
+                leaderBoard = (ArrayList<ArrayList>) input.readObject();
+                inputStream.close();
+            }
+        } catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        } catch (ClassNotFoundException e) {
+            Log.e("login activity", "File contained unexpected data type: " + e.toString());
+        } finally {
+            if (leaderBoard == null) {
+                leaderBoard = new ArrayList<>();
+            }
+        }
+    }
+
+    /**
+     * Save the scoreboard to fileName.
+     *
+     * @param fileName the name of the file
+     */
+    public void saveToScoreFile(Context context, String fileName) {
+        try {
+            ObjectOutputStream outputStream = new ObjectOutputStream(
+                    context.openFileOutput(fileName, MODE_PRIVATE));
+            outputStream.writeObject(leaderBoard);
+            outputStream.close();
+        } catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+}
