10a53aba62d6e461da74d14e636072f13433f8cc
Falanke21 <1065314944@qq.com>
2018-11-28 16:53:09 -0500

    generate tests for sudoku

---
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/game_centre/sudoku/SudokuGameControllerTest.java
@@ -0,0 +1,34 @@
+package fall2018.csc2017.game_centre.sudoku;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SudokuGameControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void setGameState() {
+    }
+
+    @Test
+    public void answerButtonClicked() {
+    }
+
+    @Test
+    public void processTapMovement() {
+    }
+
+    @Test
+    public void hint() {
+    }
+}
---
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/game_centre/sudoku/SudokuGeneratorTest.java
@@ -0,0 +1,12 @@
+package fall2018.csc2017.game_centre.sudoku;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SudokuGeneratorTest {
+
+    @Test
+    public void generate() {
+    }
+}
